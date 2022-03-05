587b316a03da3c80d9add4729a1b3ef2c627e263
EC2 Default User <ec2-user@ip-172-31-30-115.ec2.internal>
2022-03-05 04:34:53 +0000

    [Add]items controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   }
   namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
   end
   #顧客用
   #URL/customers/sign_in
